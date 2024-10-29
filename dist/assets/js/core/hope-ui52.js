e79381f9e21b4cdfc5dadca62e68abb24c9fad52
radoslaw <radoslaw.redzik@ggsitc.com>
2024-10-29 12:12:12 +0100

    Update and rename hope-ui51.js to hope-ui52.js

--- dist/assets/js/core/hope-ui52.js
+++ dist/assets/js/core/hope-ui52.js
@@ -322,7 +322,7 @@ const sidebarToggle = (elem) => {
         } else {
             sidebar.classList.add('sidebar-mini');
         }
-        setTimeout(() => { updateSidebarScrollbarHeight(); }, 1000);
+        setTimeout(() => { updateSidebarScrollbarHeight(); }, 500);
         
     })
 }
