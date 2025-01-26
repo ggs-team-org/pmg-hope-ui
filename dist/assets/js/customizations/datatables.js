59bdee1218e71d5b83333ce5e8c2ad1ac0e6c74c
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2025-01-26 15:45:51 +0100

    Customizations datatables

--- dist/assets/js/customizations/datatables.js
+++ dist/assets/js/customizations/datatables.js
@@ -1,3 +1,23 @@
 DataTable.defaults.layout.bottomStart = ['info', 'pageLength'];
 DataTable.defaults.layout.topStart = undefined;
-DataTable.ext.classes.length.select = "form-select form-select-sm";
+
+$.extend( true, DataTable.ext.classes, {
+    container: "dt-container form-inline dt-bootstrap",
+    search: {
+        input: "form-control input-sm"
+    },
+    length: {
+        select: "form-select form-select-sm"
+    },
+    processing: {
+        container: "dt-processing panel panel-default"
+    },
+    layout: {
+        row: 'row dt-layout-row',
+        cell: 'dt-layout-cell',
+        tableCell: 'col-12',
+        start: 'dt-layout-start col-sm-6',
+        end: 'dt-layout-end col-sm-6',
+        full: 'dt-layout-full col-sm-12'
+    }
+} );
